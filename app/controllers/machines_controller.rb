3a84b9ced205a95c0b8032d95ce729d9d4c570b5
Kevin Korte <kevinkorte15@gmail.com>
2015-10-20 05:04:59 +0000

    added multiple searches

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,6 +1,7 @@
 class MachinesController < ApplicationController
     
     def show
+        @machine = Machine.friendly.find(params[:id])
     end
     
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,7 @@
 class StaticController < ApplicationController
     
     def home
-        
+        @machines = Machine.all.count
+        @parts = Part.all.count
     end
 end
